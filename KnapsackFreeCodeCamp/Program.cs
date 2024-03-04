63a53efb4bee8d6a385ea1b127a477da7880c686
ErykoItaliano <eryk5683@gmail.com>
2024-03-04 13:55:59 +0100

    Blank KnapsackFreeCodeCamp

---
+++ KnapsackFreeCodeCamp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KnapsackFreeCodeCamp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
