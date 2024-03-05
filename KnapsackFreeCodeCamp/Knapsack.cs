32e7fe2840d5f86bb72a06a2453598d6bf846b87
ErykoItaliano <eryk5683@gmail.com>
2024-03-05 15:39:49 +0100

    Added Knapsack class
    Added the Knapsack class with Max_value function to calculate the maximum value that can be stored in the knapsack of a certain capacity.

---
+++ KnapsackFreeCodeCamp/Knapsack.cs
@@ -0,0 +1,38 @@
+﻿namespace KnapsackFreeCodeCamp
+{
+    internal class Knapsack(int capacity)
+    {
+        public int maxCapacity = capacity;
+
+        public int Max_value(int numberOfItems, int[] weights, int[] values)
+        {
+            int[,] data = new int[numberOfItems + 1, maxCapacity + 1];
+
+            for (int itemNum = 0; itemNum <= numberOfItems; itemNum++)
+            {
+                for (int capacity = 0; capacity <= maxCapacity; capacity++)
+                {
+                    // For the first row and collumn
+                    if (itemNum == 0 || capacity == 0)
+                    {
+                        data[itemNum, capacity] = 0;
+                    }
+                    // Find the max value
+                    else if (weights[itemNum] <= capacity)
+                    {
+                        data[itemNum, capacity] = Math.Max(values[itemNum] + data[itemNum - 1, capacity - weights[itemNum]],
+                                                                             data[itemNum - 1, capacity]);
+                    }
+                    // Max value form previous row because weight doesn't fit capacity
+                    else
+                    {
+                        data[itemNum, capacity] = data[itemNum - 1, capacity];
+                    }
+                }
+            }
+            return data[numberOfItems, maxCapacity];
+        }
+
+
+    }
+}
