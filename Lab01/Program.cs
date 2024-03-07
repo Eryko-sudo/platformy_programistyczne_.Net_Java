720cd09297b7bb61dafbfc0acf60e666ecbb6f77
ErykoItaliano <eryk5683@gmail.com>
2024-03-07 23:01:31 +0100

    Reconstruction of Knapsack program
    Reconstruction of the classes according to the instructions of the lecturer guidelines.

---
+++ Lab01/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var MyUserInput = new UserInput();
+            MyUserInput.GetUserInput();
+
+            var MyKnapsack = new KnapsackProblem(MyUserInput.knapsackSize, MyUserInput.seed);
+            var MyResult = new Result(KnapsackProblem.SolveKnapsack(KnapsackProblem.CreateRandomList(MyUserInput.itemCount, MyUserInput.seed), MyKnapsack));
+
+            //MyResult.PrintKnapsackContents(MyKnapsack);
+        }
+    }
+}
