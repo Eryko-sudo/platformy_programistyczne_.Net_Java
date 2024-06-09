8d49fcb094e8f6bb77ef9ab48d3b6de0158e77e3
ErykoItaliano <eryk5683@gmail.com>
2024-06-09 22:31:38 +0200

    Main page with clickable details

--- JAVA/library/src/main/java/org/example/library/controller/BookController.java
+++ JAVA/library/src/main/java/org/example/library/controller/BookController.java
@@ -3,20 +3,25 @@ package org.example.library.controller;
 import org.example.library.model.Book;
 import org.example.library.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
-@RestController
-@RequestMapping("/book")
+@Controller
 public class BookController {
 
     @Autowired
     private BookRepository bookRepository;
-    @GetMapping("/getAll")
-    public List<Book> getAllCategories() {
-        return bookRepository.findAll();
+
+    @GetMapping("/book/{id}")
+    public String getBookDetails(@PathVariable("id") Integer id, Model model) {
+        Book book = bookRepository.findById(id).orElse(null);
+        if (book != null) {
+            model.addAttribute("book", book);
+            return "book";
+        } else {
+            return "error"; // return to an error page or similar if the book is not found
+        }
     }
 }
--- JAVA/library/src/main/java/org/example/library/controller/IndexController.java
+++ JAVA/library/src/main/java/org/example/library/controller/IndexController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 import java.util.Collections;
@@ -20,7 +21,14 @@ public class IndexController {
     public String index(Model model) {
         List<Book> books = bookRepository.findAll();
         Collections.shuffle(books);
-        model.addAttribute("books", books.subList(0, Math.min(books.size(), 5)));
+        model.addAttribute("books", books.subList(0, Math.min(books.size(), 3)));
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam("query") String query, Model model) {
+        List<Book> books = bookRepository.search(query);
+        model.addAttribute("books", books);
         return "index";
     }
 }
