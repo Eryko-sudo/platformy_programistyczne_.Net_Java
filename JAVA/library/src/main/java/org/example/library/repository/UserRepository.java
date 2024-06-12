1e2501f62d86fe340499e94c60ce0b4e20468f7d
ErykoItaliano <eryk5683@gmail.com>
2024-06-12 14:26:58 +0200

    Add login function

--- JAVA/library/src/main/java/org/example/library/repository/UserRepository.java
+++ JAVA/library/src/main/java/org/example/library/repository/UserRepository.java
@@ -6,5 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    User findByLoginAndPassword(String login, String password);
 }
+
+
